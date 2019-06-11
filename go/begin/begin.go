4c9c64ab581b4c88325e86e12f2fcbed45c01b53
Wojciech Gac <wojciech.s.gac@gmail.com>
2019-06-11 17:58:41 +0200

    Get until Begin7 in Golang

--- go/begin/begin.go
+++ go/begin/begin.go
@@ -1,1 +1,40 @@
 package begin
+
+// Begin1 calculates the perimeter of a square based on its side
+func Begin1(a float64) float64 {
+	return a * 4.0
+}
+
+// Begin2 calculates the surface area of a square based on its side
+func Begin2(a float64) float64 {
+	return a * a
+}
+
+// Begin3 calculates the perimeter of a rectangle based on its sides
+func Begin3(a, b float64) float64 {
+	return 2.0 * (a + b)
+}
+
+// Begin4 calculates the approximate circumference of a circle based on its diameter
+func Begin4(d float64) float64 {
+	return 3.14 * d
+}
+
+// Begin5 calculates the volume and surface area of a cube based on its side
+func Begin5(a float64) (float64, float64) {
+	face := a * a
+	return face * a, 6.0 * face
+}
+
+// Begin6 calculates the volume and surface area of a parallelpiped based on its sides
+func Begin6(a, b, c float64) (float64, float64) {
+	face := a * b
+	return face * c, 2.0 * (face + b*c + a*c)
+}
+
+// Begin7 calculates the circumference and surface area of a circle based on its radius
+func Begin7(r float64) (float64, float64) {
+	// Calculate once to save one multiplication
+	PiR := 3.14 * r
+	return 2 * PiR, PiR * r
+}
--- go/begin/begin_test.go
+++ go/begin/begin_test.go
@@ -1,1 +1,141 @@
 package begin
+
+import (
+	"math"
+	"reflect"
+	"testing"
+)
+
+func TestBegin1(t *testing.T) {
+	tests := []struct {
+		input, expected float64
+	}{
+		{1.0, 4.0},
+		{10.0, 40.0},
+		{2.0, 8.0},
+		{123.0, 492.0},
+	}
+	for _, test := range tests {
+		output := Begin1(test.input)
+		if output != test.expected {
+			t.Fatalf("Expected: %v Got: %v", test.expected, output)
+		}
+	}
+}
+
+func TestBegin2(t *testing.T) {
+	tests := []struct {
+		input, expected float64
+	}{
+		{1.0, 1.0},
+		{10.0, 100.0},
+		{2.0, 4.0},
+		{123.0, 15129.0},
+	}
+	for _, test := range tests {
+		output := Begin2(test.input)
+		if output != test.expected {
+			t.Fatalf("Expected: %v Got: %v", test.expected, output)
+		}
+	}
+}
+
+func TestBegin3(t *testing.T) {
+	tests := []struct {
+		input    []float64
+		expected float64
+	}{
+		{[]float64{1.0, 2.0}, 6.0},
+		{[]float64{10.0, 20.0}, 60.0},
+		{[]float64{2.0, 7.0}, 18.0},
+		{[]float64{123.0, 100.0}, 446.0},
+	}
+	for _, test := range tests {
+		fun := reflect.ValueOf(Begin3)
+		var args []reflect.Value
+		for _, arg := range test.input {
+			args = append(args, reflect.ValueOf(arg))
+		}
+		res := fun.Call(args)
+		output := res[0].Interface().(float64)
+		if output != test.expected {
+			t.Fatalf("Expected: %v Got: %v", test.expected, output)
+		}
+	}
+}
+
+func TestBegin4(t *testing.T) {
+	tests := []struct {
+		input, expected float64
+	}{
+		{1.0, 3.14},
+		{2.0, 6.28},
+		{10.0, 31.4},
+		{123.0, 386.22},
+	}
+	for _, test := range tests {
+		output := Begin4(test.input)
+		// Approximate equality due to float rounding
+		if math.Abs(output-test.expected) > 1e-9 {
+			t.Fatalf("Expected: %v Got: %v", test.expected, output)
+		}
+	}
+}
+
+func TestBegin5(t *testing.T) {
+	tests := []struct {
+		input    float64
+		expected []float64
+	}{
+		{1.0, []float64{1.0, 6.0}},
+		{2.0, []float64{8.0, 24.0}},
+		{10.0, []float64{1000.0, 600.0}},
+		{123.0, []float64{1860867.0, 90774.0}},
+	}
+	for _, test := range tests {
+		vol, area := Begin5(test.input)
+		// Approximate equality due to float rounding
+		if vol != test.expected[0] || area != test.expected[1] {
+			t.Fatalf("Expected: %v Got: %v", test.expected, []float64{vol, area})
+		}
+	}
+}
+
+func TestBegin6(t *testing.T) {
+	tests := []struct {
+		input    []float64
+		expected []float64
+	}{
+		{[]float64{1.0, 2.0, 3.0}, []float64{6.0, 22.0}},
+		{[]float64{2.0, 4.0, 6.0}, []float64{48.0, 88.0}},
+		{[]float64{10.0, 10.0, 20.0}, []float64{2000.0, 1000.0}},
+		{[]float64{123.0, 128.0, 182.0}, []float64{2.865408e+06, 122852.0}},
+	}
+	for _, test := range tests {
+		vol, area := Begin6(test.input[0], test.input[1], test.input[2])
+		// Approximate equality due to float rounding
+		if vol != test.expected[0] || area != test.expected[1] {
+			t.Fatalf("Expected: %v Got: %v", test.expected, []float64{vol, area})
+		}
+	}
+}
+
+func TestBegin7(t *testing.T) {
+	tests := []struct {
+		input    float64
+		expected []float64
+	}{
+		{1.0, []float64{6.28, 3.14}},
+		{2.0, []float64{12.56, 12.56}},
+		{10.0, []float64{62.8, 314.0}},
+		{123.0, []float64{772.44, 47505.06}},
+	}
+	for _, test := range tests {
+		circ, area := Begin7(test.input)
+		// Approximate equality due to float rounding
+		if math.Abs(circ-test.expected[0]) > 1e-9 ||
+			math.Abs(area-test.expected[1]) > 1e-9 {
+			t.Fatalf("Expected: %v Got: %v", test.expected, []float64{circ, area})
+		}
+	}
+}
