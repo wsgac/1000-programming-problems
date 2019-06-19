9d44b77af98e128041857263b8cf404befa1ba4f
Wojciech Gac <wojciech.s.gac@gmail.com>
2019-06-19 17:23:51 +0200

    Get up to Boolean29 (with incomplete tests)

--- go/boolean/boolean.go
+++ go/boolean/boolean.go
@@ -94,3 +94,90 @@ func Boolean15(a, b, c int64) bool {
 	}
 	return cnt == 2
 }
+
+// Boolean16 checks if its argument is a two-digit even number
+func Boolean16(n int64) bool {
+	return n > 9 && n < 100 && n%2 == 0
+}
+
+// Boolean17 checks if its argument is a three-digit odd number
+func Boolean17(n int64) bool {
+	return n > 99 && n < 1000 && n%2 == 1
+}
+
+// Boolean18 checks if at least two of its arguments are equal
+func Boolean18(a, b, c int64) bool {
+	set := make(map[int64]bool)
+	set[a] = true
+	set[b] = true
+	set[c] = true
+	return len(set) < 3
+}
+
+// Boolean19 checks if at least two of its arguments are mutually opposite
+func Boolean19(a, b, c int64) bool {
+	return a == -b || a == -c || b == -c
+}
+
+// Boolean20 checks if all digits of a three digit number are different from each other
+func Boolean20(n int64) bool {
+	set := make(map[int64]bool)
+	for i := 0; i < 3; i++ {
+		set[n%10] = true
+		n /= 10
+	}
+	return len(set) == 3
+}
+
+// Boolean21 checks if the digits of a three-digit number form an increasing sequence
+func Boolean21(n int64) bool {
+	return n%10 > n/10%10 && n/10%10 > n/100
+}
+
+// Boolean22 checks if the digits of a three-digit nubmer form a monotonic sequence
+func Boolean22(n int64) bool {
+	return (n%10 > n/10%10 && n/10%10 > n/100) ||
+		(n%10 < n/10%10 && n/10%10 < n/100)
+}
+
+// Boolean23 checks if a four-digit number is a palindrome
+func Boolean23(n int64) bool {
+	str := string(n)
+	return str[0] == str[3] && str[1] == str[2]
+}
+
+// Boolean24 checks if a quadratic equation has real roots
+func Boolean24(a, b, c int64) bool {
+	d := b*b - 4*a*c
+	return d >= 0
+}
+
+// Boolean25 checks if point (x,y) lies in the 2nd quarter of the coordinate system
+func Boolean25(x, y int64) bool {
+	return x < 0 && y > 0
+}
+
+// Boolean26 checks if point (x,y) lies in the 4th quarter of the coordinate system
+func Boolean26(x, y int64) bool {
+	return x > 0 && y < 0
+}
+
+// Boolean27 checks if point (x,y) lies in the 3rd quarter of the coordinate system
+func Boolean27(x, y int64) bool {
+	return x < 0
+}
+
+// Boolean28 checks if point (x,y) lies in the 1st or 3rd quarter of the coordinate system
+func Boolean28(x, y int64) bool {
+	return (x > 0 && y > 0) || (x < 0 && y < 0)
+}
+
+// Boolean29 checks if point (x,y) lies inside a rectangle spanned by (x1,y1) and (x2,y2)
+func Boolean29(x, y, x1, y1, x2, y2) bool {
+	return x1 < x && x < x2 && y2 < y && y < y1
+}
+
+// Boolean30
+func Boolean30(a, b, c int64) bool {
+
+}
--- go/boolean/boolean_test.go
+++ go/boolean/boolean_test.go
@@ -19,3 +19,39 @@ func TestBoolean11(t *testing.T) {
 		}
 	}
 }
+
+func TestBoolean18(t *testing.T) {
+	tests := []struct {
+		input  []int64
+		output bool
+	}{
+		{[]int64{1, 2, 3}, false},
+		{[]int64{1, 1, 2}, true},
+		{[]int64{1, 2, 2}, true},
+		{[]int64{1, 1, 1}, true},
+	}
+	for _, test := range tests {
+		output := Boolean18(test.input[0], test.input[1], test.input[2])
+		if output != test.output {
+			t.Errorf("Expected: %v Got: %v", test.output, output)
+		}
+	}
+}
+
+func TestBoolean20(t *testing.T) {
+	tests := []struct {
+		input  int64
+		output bool
+	}{
+		{123, true},
+		{112, false},
+		{122, false},
+		{111, false},
+	}
+	for _, test := range tests {
+		output := Boolean20(test.input)
+		if output != test.output {
+			t.Errorf("Expected: %v Got: %v", test.output, output)
+		}
+	}
+}
